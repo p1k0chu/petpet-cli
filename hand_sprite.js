d5f26d01b0b8e2f066a415dee873ed02c6c764c7
p1k0chu <p1k0chu.vim@gmail.com>
2026-01-18 22:42:51 +0200

    move half of the code to separate file

---
+++ hand_sprite.js
@@ -0,0 +1,55 @@
+import {loadImage} from "canvas"
+
+export const g = {
+  x: 18,
+  y: 18,
+  w: 112,
+  h: 112,
+  scale: 0.875,
+};
+
+export const frames = [
+  {
+    x: g.x,
+    y: g.y,
+    w: g.w * g.scale,
+    h: g.h * g.scale,
+  },
+  {
+    x: g.x - 4,
+    y: g.y + 12,
+    w: g.w * g.scale + 4,
+    h: g.h * g.scale - 12,
+  },
+  {
+    x: g.x - 12,
+    y: g.y + 18,
+    w: g.w * g.scale + 12,
+    h: g.h * g.scale - 18,
+  },
+  {
+    x: g.x - 12,
+    y: g.y + 12,
+    w: g.w * g.scale + 4,
+    h: g.h * g.scale - 12,
+  },
+  {
+    x: g.x - 4,
+    y: g.y,
+    w: g.w * g.scale,
+    h: g.h * g.scale,
+  },
+];
+
+/** Remove partially transparent & #00ff00 (bg color) green pixels */
+export function optimizeFrameColors(data) {
+  for (let i = 0; i < data.length; i += 4) {
+    // clamp greens to avoid pure greens from turning transparent
+    data[i + 1] = data[i + 1] > 250 ? 250 : data[i + 1];
+    // clamp transparency
+    data[i + 3] = data[i + 3] > 127 ? 255 : 0;
+  }
+}
+
+export const handSpritePromise = loadImage("img/sprite.png");
+
--- index.js
+++ index.js
@@ -1,64 +1,12 @@
 import { createCanvas, Image, loadImage } from "canvas";
 import GifEncoder from "gif-encoder";
 import { createWriteStream } from "fs";
-
-const g = {
-  x: 18,
-  y: 18,
-  w: 112,
-  h: 112,
-  scale: 0.875,
-};
-
-const frames = [
-  {
-    x: g.x,
-    y: g.y,
-    w: g.w * g.scale,
-    h: g.h * g.scale,
-  },
-  {
-    x: g.x - 4,
-    y: g.y + 12,
-    w: g.w * g.scale + 4,
-    h: g.h * g.scale - 12,
-  },
-  {
-    x: g.x - 12,
-    y: g.y + 18,
-    w: g.w * g.scale + 12,
-    h: g.h * g.scale - 18,
-  },
-  {
-    x: g.x - 12,
-    y: g.y + 12,
-    w: g.w * g.scale + 4,
-    h: g.h * g.scale - 12,
-  },
-  {
-    x: g.x - 4,
-    y: g.y,
-    w: g.w * g.scale,
-    h: g.h * g.scale,
-  },
-];
-
-/** Remove partially transparent & #00ff00 (bg color) green pixels */
-function optimizeFrameColors(data) {
-  for (let i = 0; i < data.length; i += 4) {
-    // clamp greens to avoid pure greens from turning transparent
-    data[i + 1] = data[i + 1] > 250 ? 250 : data[i + 1];
-    // clamp transparency
-    data[i + 3] = data[i + 3] > 127 ? 255 : 0;
-  }
-}
-
-const handSprite = await loadImage("img/sprite.png");
+import { g, frames, optimizeFrameColors, handSpritePromise } from "./hand_Sprite.js"
 
 const SIZE = 112;
 
 const filePath = process.argv[2];
-const userImage = await loadImage(filePath);
+const userImagePromise = loadImage(filePath);
 
 const canvas = createCanvas(SIZE, SIZE);
 const ctx = canvas.getContext("2d");
@@ -72,6 +20,9 @@ gif.setRepeat(0); // 0 -> infinite loop
 gif.setDelay(63); // delay in ms
 
 gif.writeHeader();
+
+const handSprite = await handSpritePromise
+const userImage = await userImagePromise
 
 for (let i = 0; i < frames.length; i++) {
   const frame = frames[i];
